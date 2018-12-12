5e3aa350998d5ee478a0a7243dd3d8c619542017
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2018-12-12 18:13:57 -0500

    Clean up on format of code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,15 @@
 var foods = ["pancakes", "pizza", "sushi", "waffles", "bacon", "tacos", "cheeseburger"];
 
 function displayGIFs() {
-    
+    var APIkey = "Fj3ngqeuQuyw4vMKcpLOLoipExrX5aJD";
+    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=" + APIkey + "&tag=" + food;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+
+    .then(function(response) {
+        
+    })
 }
